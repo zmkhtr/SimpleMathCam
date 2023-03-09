3f499513f192fc3feeca50396e2aee2d3aad3c8c
zmkhtr <azammukhtar3@gmail.com>
2023-03-09 20:33:32 +0700

    Initial Commit

---
+++ SimpleMathCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMathCam
+//
+//  Created by Azam Mukhtar on 09/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
