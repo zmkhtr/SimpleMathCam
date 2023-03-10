92e211e720f8bbb601cbb97720ffe66328c60075
zmkhtr <azammukhtar3@gmail.com>
2023-03-10 09:18:32 +0700

    Move Test Helper to specific file and folders

--- SimpleMathCamTests/MathTextExtractorTests.swift
+++ SimpleMathCamTests/MathTextExtractorTests.swift
@@ -184,24 +184,3 @@ class MathTextExtractorTests: XCTestCase {
     }
 }
 
-extension XCTestCase {
-    
-    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
-}
-
-extension UIImage {
-    static func make(withColor color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()!
-        context.setFillColor(color.cgColor)
-        context.fill(rect)
-        let img = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return img!
-    }
-}
