9590e91a790457e6474dcf14488b0741468809e2
zmkhtr <azammukhtar3@gmail.com>
2023-03-10 09:17:22 +0700

    Create MLKitTextRecognizerTests

---
+++ SimpleMathCamTests/MLKitTextRecognizerTests.swift
@@ -0,0 +1,16 @@
+//
+//  MLKitTextRecognizerTests.swift
+//  SimpleMathCamTests
+//
+//  Created by Azam Mukhtar on 10/03/23.
+//
+
+import XCTest
+@testable import SimpleMathCam
+
+class MLKitTextRecognizerTests: XCTestCase  {
+    
+    func test_fail() {
+        XCTFail("Test Fail")
+    }
+}
